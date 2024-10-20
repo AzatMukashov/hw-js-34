10743d89857b9d1969cb0fa32375a8c0fc2840c6
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-21 01:00:58 +0600

    поменял цвет навигационному бару добавил на мой взгляд более логичную переадресацию на название сайта добавил контейнеры последнему отступ

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import './App.css'
-import { NavLink, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink, Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import Home from './components/Home.tsx';
 import About from './components/About.tsx';
 import Contact from './components/Contact.tsx';
+import './App.css'
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#">Мой вебсайт</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={NavLink} to="/" end>Дом</Nav.Link>
-              <Nav.Link as={NavLink} to="/about" end>О нас</Nav.Link>
-              <Nav.Link as={NavLink} to="/contact" end>Контакт</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+        <Navbar bg="primary" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to='/'>Мой вебсайт</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link as={NavLink} to="/" end>Дом</Nav.Link>
+                <Nav.Link as={NavLink} to="/about" end>О нас</Nav.Link>
+                <Nav.Link as={NavLink} to="/contact" end>Контакт</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-        </Routes>
+        <Container className='mt-4'>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </Container>
       </div>
     </Router>
   )
